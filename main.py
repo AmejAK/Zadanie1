4ca235f6b40aa96967c930457f646a01e10cbede
Arkadiusz Majczyna <arek36912@gmail.com>
2020-11-24 19:24:30 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+x=0
+while x<6 :
+    slowo = input("Podaj słowo")
+    if slowo=='kot':
+        print("gartulacje")
+        break
+    if x==4 :
+        p=print("przegrales")
+        break
+    else:
+        x=x+1
+
+
